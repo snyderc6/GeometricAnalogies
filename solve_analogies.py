b7f3e6d856a71717fbc4ac31130c85801a1410c4
Caitlin <caitlin.snyder24@gmail.com>
2018-09-13 17:20:57 -0500

    Splitting up functions to help with rotation stuff

--- solve_analogies.py
+++ solve_analogies.py
@@ -16,27 +16,38 @@ import numpy as np
 # function to read in all 8 images from problem
 def read_in_images(path):
 
-    # read in analogy images
-    A = np.array(Image.open(os.path.join(path, 'a.gif')).convert('1', dither=Image.NONE))
-    B = np.array(Image.open(os.path.join(path, 'b.gif')).convert('1', dither=Image.NONE))
-    A = 1 - A
-    B = 1 - B
+    # read in images
+    A = Image.open(os.path.join(path, 'a.gif'))
+    B = Image.open(os.path.join(path, 'b.gif'))
+    C = Image.open(os.path.join(path, 'c.gif'))
+    a1 = Image.open(os.path.join(path, '1.gif'))
+    a2 = Image.open(os.path.join(path, '2.gif'))
+    a3 = Image.open(os.path.join(path, '3.gif'))
+    a4 = Image.open(os.path.join(path, '4.gif'))
+    a5 = Image.open(os.path.join(path, '5.gif'))
+    return A, B, C, a1, a2, a3, a4, a5
+
+# function that converts image to array map
+def convert_to_array_map(image):
+    array_map = np.array(image.convert('1', dither=Image.NONE))
+    array_map = 1 - array_map
+    return array_map
+
+# function to get images to array maps
+def images_to_array_maps(A, B, C, a1, a2, a3, a4, a5):
+     # read in analogy images
+    A = convert_to_array_map(A)
+    B = convert_to_array_map(B)
 
     # read in problem image
-    C = np.array(Image.open(os.path.join(path, 'c.gif')).convert('1', dither=Image.NONE))
-    C = 1 - C
+    C = convert_to_array_map(C)
 
     # read in answer images
-    a1 = np.array(Image.open(os.path.join(path, '1.gif')).convert('1', dither=Image.NONE))
-    a2 = np.array(Image.open(os.path.join(path, '2.gif')).convert('1', dither=Image.NONE))
-    a3 = np.array(Image.open(os.path.join(path, '3.gif')).convert('1', dither=Image.NONE))
-    a4 = np.array(Image.open(os.path.join(path, '4.gif')).convert('1', dither=Image.NONE))
-    a5 = np.array(Image.open(os.path.join(path, '5.gif')).convert('1', dither=Image.NONE))
-    a1 = 1 - a1
-    a2 = 1 - a2
-    a3 = 1 - a3
-    a4 = 1 - a4
-    a5 = 1 - a5
+    a1 = convert_to_array_map(a1)
+    a2 = convert_to_array_map(a2)
+    a3 = convert_to_array_map(a3)
+    a4 = convert_to_array_map(a4)
+    a5 = convert_to_array_map(a5)
 
     # plt.imshow(A, cmap='binary')
     # plt.show()
@@ -46,7 +57,8 @@ def read_in_images(path):
 
 # function to analyze other differences, split into quadrants, and find best answer
 def analyze_differences(A, B, C, a1, a2, a3, a4, a5):
-
+    #images to array maps
+    A, B, C, a1, a2, a3, a4, a5 = images_to_array_maps(A, B, C, a1, a2, a3, a4, a5)
     # calculate difference between maps
     rows = A.shape[0]
     cols = A.shape[1]
@@ -96,6 +108,16 @@ def best_answer(compare_map):
 
     return min_idx
 
+# function that attempts to rotate the image A to match image B
+# returns the degree with the highest proportion of matching 
+# pixels after rotation degrees: 45, 90, 135, 180, 225, 270, 315 
+# and the actual proportion
+def check_rotation(A, B):
+
+    # rotate A
+    A_rotated = A.rotate(45)
+    # count nm
+
 # function that solves problems 
 def solve_problem(problem):
     #basedir = '/Users/deirdre/Documents/ImageryAI/Assignment1/'
@@ -127,16 +149,19 @@ def print_answers(answers):
 # Main function to solve geometric analogy problems
 def main():
     correct_answers = [2,2,4,4,3,1,1,2,5,5,3,3,2,4,2]
+    total_correct = 0
     for i in range(1,16):
-        problem = 'm'
-        problem = problem + str(i)
+        problem = 'm' + str(i)
         answers = solve_problem(problem)
         if(answers[0] == correct_answers[i-1]):
+            total_correct = total_correct + 1
             #system got the answer correct
             print("Problem " + problem + " answered correctly!")
         else:
-            print("Problem " + problem + " answers in order of highest matching:")
+            #system got the answer wrong
+            print("Problem " + problem + " answers (correct answer is " + str(correct_answers[i-1]) + '):')
             print_answers(answers)
+    print("Total Correct: " + str(total_correct))
     ################ ANSWERS ###############
     # Problem 1 Answer: 2 *
     # Problem 2 Answer: 2 *
